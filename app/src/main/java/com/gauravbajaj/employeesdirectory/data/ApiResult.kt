4c33b6e26ae37f856143f3d169a69c7aba0c2d0c
Gaurav Bajaj <letsdecode@gmail.com>
2025-07-23 22:14:50 -0700

    feat: create ApiResult sealed class for state management

---
+++ app/src/main/java/com/gauravbajaj/employeesdirectory/data/ApiResult.kt
@@ -0,0 +1,7 @@
+package com.gauravbajaj.employeesdirectory.data
+
+sealed class ApiResult<T> {
+    data class Success<T>(val data: T) : ApiResult<T>()
+    data class Error<T>(val message: String) : ApiResult<T>()
+    class Loading<T> : ApiResult<T>()
+}
