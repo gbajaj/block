201ebf8220f09d27be6335a61f09b506f831718e
Gaurav Bajaj <letsdecode@gmail.com>
2025-07-23 22:03:43 -0700

    feat: create Employee data model with all required fields

--- app/src/main/java/com/gauravbajaj/employeesdirectory/data/model/Employee.kt
+++ app/src/main/java/com/gauravbajaj/employeesdirectory/data/model/Employee.kt
@@ -15,6 +15,15 @@ import kotlinx.parcelize.Parcelize
 data class Employee(
     val uuid: String,
     val full_name: String,
+    val phone_number: String?,
     val email_address: String,
-    val photo_url_small: String? = null
-) : Parcelable
+    val biography: String?,
+    val photo_url_small: String?,
+    val photo_url_large: String?,
+    val team: String,
+    val employee_type: EmployeeType
+): Parcelable
+
+enum class EmployeeType {
+    FULL_TIME, PART_TIME, CONTRACTOR
+}
